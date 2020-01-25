5b22cc68a02df18757236ecd6141ca3945bb8e9c
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-01-24 21:46:34 -0500

    created start button in html

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+// Creates the variables
+var start = document.getElementById("start");
+
+
+var questions = 
+{q: "Who is the host of Watch What Happens Live", a: "Andy Cohen"},
+}
+
+
+{q: "Which cast of housewives begun the Real Housewives franchise?", a: "Real Housewives of Orange County"},
+
+)};
